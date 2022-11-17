52d356a31b54ddc9ed7c0eac93ad9826452de145
laulloliv <laulloliv@gmail.com>
2022-11-16 23:28:47 -0400

    models

---
+++ src/main/java/com/br/ifam/TodoApp/model/Categoria.java
@@ -0,0 +1,55 @@
+package com.br.ifam.TodoApp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Categoria {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String titulo;
+    @Column()
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(String titulo, String descricao) {
+        setTitulo(titulo);
+        setDescricao(descricao);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
